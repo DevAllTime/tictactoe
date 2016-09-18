2eeeffe82a7e6050e9f7326883ae0e44d785dcf8
Santhosh Suram <suramsanthoshkumar@gmail.com>
2016-09-18 15:15:57 -0400

    Added more functinality

--- TTTgame.py
+++ TTTgame.py
@@ -1,5 +1,3 @@
-
-
 #Tic Tac Toe game in python by #Syntax.
 #enter elements 0,2,4
 
@@ -40,43 +38,44 @@ def setting_inputs(row,coloumn,valu):
 
 def didyouloose():
     #first 3 about horizontal equal,next 2 diogonal, next 3 vertical equals
-    if(first_row[0] == first_row[1] == first_row[2] and first_row[0]!= None and first_row[1]!= None and first_row[2]!= None or \
-    second_row[0] == second_row[1] == second_row[2] and second_row[0]!= None and second_row[1]!= None and second_row[2]!= None or \
-    third_row[0] == third_row[0] == third_row[2] and third_row[0]!=None and third_row[0]!=None and third_row[2]!= None  or \
-    first_row[0] == second_row[1] == third_row[2] and first_row[0]!=None and second_row[1]!=None and third_row[2]!=None or \
-    first_row[2] == second_row[1] == third_row[0] and first_row[2]!=None and second_row[1]!=None and third_row[0]!=None or \
-    first_row[0] == second_row[0] == third_row[0] and first_row[0]!=None and second_row[0]!=None and third_row[0]!=None or \
-    first_row[1] == second_row[1] == third_row[1] and first_row[1]!=None and second_row[1]!=None and third_row[1]!=None or \
-    first_row[2] == second_row[2] == third_row[2] and first_row[2]!=None and second_row[2]!=None and third_row[2]!=None ):
-        return False
-    else:
-        return True
-
     if first_row[0] == first_row[1] == first_row[2]:
         if first_row[0]!=None and first_row[1]!=None and first_row[2]!=None:
             return False
     if second_row[0] == second_row[1] == second_row[2]:
-        if second_row[0]!=None and second_row[1]!=None and second_row[2]!
+        if second_row[0]!=None and second_row[1]!=None and second_row[2]!=None:
+            return False
+    if third_row[0] == third_row[0] == third_row[2]:
+        if third_row[0]!=None and third_row[0]!=None and third_row[2]!=None:
+            return False
+    if first_row[0] == second_row[1] == third_row[2]:
+        if first_row[0]!=None and second_row[1]!=None and third_row[2]!=None:
+            return False
+    if first_row[2] == second_row[1] == third_row[0]:
+        if first_row[2]!=None and second_row[1]!=None and third_row[0]!=None:
+            return False
+    if first_row[0] == second_row[0] == third_row[0]:
+        if first_row[0]!=None and second_row[0]!=None and third_row[0]!=None:
+            return False
+    if first_row[1] == second_row[1] == third_row[1]:
+        if first_row[1]!=None and second_row[1]!=None and third_row[1]!=None:
+            return False
+    if first_row[2] == second_row[2] == third_row[2]:
+        if first_row[2]!=None and second_row[2]!=None and third_row[2]!=None:
+            return False
+    return True
 
-
-
-
-#********************************MAIN FUNCTION******************************
+#********************************MAIN FUNCTION******************************#
 print "Welcome to the Tic Tac Toe Game!!\n"
 print "Please Enter the values accroding to"
 print "00,01,02|10,11,12|20,21,22"
 result = True
 #while(result):
-for i in range(3):
+while(result):
     r_ow = raw_input("Enter the Row")
     c_ol = raw_input("Enter the Coloumn")
     i_nput = raw_input("Enter the input value")
     setting_inputs(int(r_ow),int(c_ol),int(i_nput))
     display_status()
-    #result = didyouloose()
-    if(not didyouloose()):
-        print "WON"
-
-
-
-#check if you won?
+    result = didyouloose()
+    if(not result):
+        print "%s Won the Game"%(i_nput)
