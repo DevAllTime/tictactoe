331381ceb30a06931851963ec17e9d6d8a58838c
Santhosh Suram <suramsanthoshkumar@gmail.com>
2016-09-18 14:50:59 -0400

    Added Functionality

--- TTTgame.py
+++ TTTgame.py
@@ -22,50 +22,61 @@ def display_status():
 def setting_inputs(row,coloumn,valu):
     #input elements into the list & #Elements should not be replaced
     # debugging print "%d %d %d"%(row,coloumn,valu)
-    if(row == 0 and first_row[coloumn]== 1 or 0):
-        print"You cannot Replace"
-    else:
-        first_row[coloumn] = valu
-        return
-
-    if row == 1 and second_row[coloumn]== 1 or 0:
-        print"You cannot Replace"
-    else:
-        second_row[coloumn] = valu
-        return
-
-    if row == 2 and third_row[coloumn]== 1 or 0:
-        print"You cannot replace"
-    else:
-        third_row[coloumn] = valu
-        return
+    if row == 0:
+        if first_row[coloumn]== 1 or 0:
+            print"YOU CANNOT REPLACE!!!\n"
+        else:
+            first_row[coloumn] = valu
+    if row == 1:
+        if second_row[coloumn]== 1 or 0:
+            print"YOU CANNOT REPLACE!!!\n"
+        else:
+            second_row[coloumn] = valu
+    if row == 2:
+        if third_row[coloumn]== 1 or 0:
+            print"YOU CANNOT REPLACE!!!\n"
+        else:
+            third_row[coloumn] = valu
 
 def didyouloose():
     #first 3 about horizontal equal,next 2 diogonal, next 3 vertical equals
-    if(first_row[0] == first_row[1] == first_row[2] or \
-    second_row[0] == second_row[1] == second_row[2] or \
-    third_row[0] == third_row[0] == third_row[2] or \
-    first_row[0] == second_row[1] == third_row[2] or \
-    first_row[2] == second_row[1] == third_row[0] or \
-    first_row[0] == second_row[0] == third_row[0] or \
-    first_row[1] == second_row[1] == third_row[1] or \
-    first_row[2] == second_row[2] == third_row[2]):
+    if(first_row[0] == first_row[1] == first_row[2] and first_row[0]!= None and first_row[1]!= None and first_row[2]!= None or \
+    second_row[0] == second_row[1] == second_row[2] and second_row[0]!= None and second_row[1]!= None and second_row[2]!= None or \
+    third_row[0] == third_row[0] == third_row[2] and third_row[0]!=None and third_row[0]!=None and third_row[2]!= None  or \
+    first_row[0] == second_row[1] == third_row[2] and first_row[0]!=None and second_row[1]!=None and third_row[2]!=None or \
+    first_row[2] == second_row[1] == third_row[0] and first_row[2]!=None and second_row[1]!=None and third_row[0]!=None or \
+    first_row[0] == second_row[0] == third_row[0] and first_row[0]!=None and second_row[0]!=None and third_row[0]!=None or \
+    first_row[1] == second_row[1] == third_row[1] and first_row[1]!=None and second_row[1]!=None and third_row[1]!=None or \
+    first_row[2] == second_row[2] == third_row[2] and first_row[2]!=None and second_row[2]!=None and third_row[2]!=None ):
         return False
     else:
         return True
+
+    if first_row[0] == first_row[1] == first_row[2]:
+        if first_row[0]!=None and first_row[1]!=None and first_row[2]!=None:
+            return False
+    if second_row[0] == second_row[1] == second_row[2]:
+        if second_row[0]!=None and second_row[1]!=None and second_row[2]!
+
+
 
 
 #********************************MAIN FUNCTION******************************
 print "Welcome to the Tic Tac Toe Game!!\n"
 print "Please Enter the values accroding to"
 print "00,01,02|10,11,12|20,21,22"
+result = True
+#while(result):
 for i in range(3):
     r_ow = raw_input("Enter the Row")
     c_ol = raw_input("Enter the Coloumn")
     i_nput = raw_input("Enter the input value")
     setting_inputs(int(r_ow),int(c_ol),int(i_nput))
     display_status()
-    if(didyouloose()):
-        print "you won!"
+    #result = didyouloose()
+    if(not didyouloose()):
+        print "WON"
+
+
 
 #check if you won?
