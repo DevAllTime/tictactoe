7d4917cf97f32ba0432a7b0a744e447cdcff0cda
Santhosh Suram <suramsanthoshkumar@gmail.com>
2016-10-20 19:52:54 -0400

    initial commit

--- TTTgame.py
+++ TTTgame.py
@@ -6,7 +6,7 @@ second_row = [None,None,None]
 third_row = [None,None,None]
 
 #This function displays status
-#this is the function  2
+#this is the function 2
 def display_status():
     for i in first_row:
         print "%s |"%(i),
